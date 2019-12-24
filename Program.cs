3098853c770e5c4b8aed92121fe1170bb521c113
Mikhail <mhusarenko@gloriumtech.com>
2019-12-24 14:44:24 +0200

    fix parser, new urls

--- Program.cs
+++ Program.cs
@@ -62,7 +62,7 @@ namespace Dirtywall
 
             while (true)
             {
-                WallhavenParser.parseAndSet(query, searchQuery);
+                WallhavenParser.ParseAndSet(query, searchQuery);
                 Thread.Sleep(interval * 1000 * 60);
             }
        }
@@ -109,7 +109,7 @@ namespace Dirtywall
 
             trayIcon.Icon = Resources.curr_icon;
             trayIcon.Visible = true;
-            trayIcon.ShowBalloonTip(100, "Dirty wall", "Программа запущена!", ToolTipIcon.Info);
+            //trayIcon.ShowBalloonTip(100, "Dirty wall", "Программа запущена!", ToolTipIcon.Info);
 
             Thread mainThread = new Thread(mainSegment);
             mainThread.Priority = ThreadPriority.Highest;
@@ -156,7 +156,7 @@ namespace Dirtywall
 
         private static void NextWallpaper_Click(object sender, EventArgs e)
         {
-            WallhavenParser.parseAndSet(query,searchQuery);
+            WallhavenParser.ParseAndSet(query,searchQuery);
         }
     }
 }
