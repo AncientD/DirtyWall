9996c4a7e263b9090d31425d6068c7f1a6d1a1c9
Mike <mikhail.work.box@gmail.com>
2025-12-28 12:09:24 +0200

    refactor: Rename `WallhavenParser.parseAndSet` to `WallhavenParser.ParseAndSet` for naming consistency.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -151,7 +151,7 @@ namespace Dirtywall
 
         private static void NextWallpaper_Click(object sender, EventArgs e)
         {
-            WallhavenParser.parseAndSet(query, searchQuery);
+            WallhavenParser.ParseAndSet(query, searchQuery);
         }
     }
 }
